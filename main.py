ceb075b78d53be9583ed15be8380ee2a6b264409
GediminasSiaudinis <gediminas.siaudinis@yahoo.com>
2023-12-08 08:48:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# pasirinkimas = int(
+#     input("Jei norite iveste pajamas - 1 Jei islaidas - 2 Jei paziureti balansa - 3 Jei iseiti - 4 Jei ataskaita - 5 "))
+
+septintas = input("|7|")
+astuntas = input("|8|")
+devintas = input("|9|")
