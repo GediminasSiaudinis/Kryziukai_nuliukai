2aa8c846f7ffb9f1e21ed80a332bf1c4f9df3839
GediminasSiaudinis <gediminas.siaudinis@yahoo.com>
2023-12-08 09:27:39 +0200

    Pirmas variantas

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 # pasirinkimas = int(
 #     input("Jei norite iveste pajamas - 1 Jei islaidas - 2 Jei paziureti balansa - 3 Jei iseiti - 4 Jei ataskaita - 5 "))
 
-septintas = input("|7|")
-astuntas = input("|8|")
-devintas = input("|9|")
+septintas = print("|7|", end="")
+astuntas = print("|8|", end="")
+devintas = print("|9|")
+ketvirtas = print("|4|", end="")
+penktas = print("|5|", end="")
+sestas = print("|6|")
+pirmas = print("|1|", end="")
+antras = print("|2|", end="")
+trecias = print("|3|")
+pasirinkimasX = int(input("Iveskite X langeli: "))
+
