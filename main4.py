df52aa6c6ca2a6a46082aaf1f27195da8c8874a8
GediminasSiaudinis <gediminas.siaudinis@yahoo.com>
2023-12-10 17:10:59 +0200

    Antras variantas

--- main4.py
+++ main4.py
@@ -22,15 +22,17 @@ class pasirinkimasO(pasirinkimai):
 
 class Spejimai:
     def __init__(self):
-        self.spejimai = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.spejimai = [7, 8, 9, 4, 5, 6, 1, 2, 3]
 
     def prideti_X_spejima(self, pasirinkimai):
         pasirinkimai2 = pasirinkimasX(pasirinkimai)
-        self.spejimai[pasirinkimai - 1] = "X"
+        pasirinkimai = self.spejimai.index(pasirinkimai)
+        self.spejimai[pasirinkimai] = "X"
 
     def prideti_O_spejima(self, pasirinkimai):
         pasirinkimai2 = pasirinkimasO(pasirinkimai)
-        self.spejimai[pasirinkimai - 1] = "O"
+        pasirinkimai = self.spejimai.index(pasirinkimai)
+        self.spejimai[pasirinkimai] = "O"
 
     def atvaizduoti(self):
         count = 0
@@ -42,15 +44,30 @@ class Spejimai:
                 print()
         return self.spejimai
 
-    def balansas(self):
-        suma = 0
-        for x in self.spejimai:
-            if isinstance(x, pasirinkimasX):
-                suma += x.suma
-            elif isinstance(x, pasirinkimasO):
-                suma -= x.suma
-        return suma
 
+    def ar_laimejo_X(self):
+        for x in self.spejimai:
+            if self.spejimai[0] and self.spejimai[1] and self.spejimai[2] == "X":
+                print("X laimejo")
+            elif self.spejimai[3] and self.spejimai[4] and self.spejimai[5] == "X":
+                print("X laimejo")
+            elif self.spejimai[6] and self.spejimai[7] and self.spejimai[8] == "X":
+                print("X laimejo")
+            elif self.spejimai[0] and self.spejimai[3] and self.spejimai[6] == "X":
+                print("X laimejo")
+            elif self.spejimai[1] and self.spejimai[4] and self.spejimai[7] == "X":
+                print("X laimejo")
+            elif self.spejimai[2] and self.spejimai[5] and self.spejimai[8] == "X":
+                print("X laimejo")
+            elif self.spejimai[0] and self.spejimai[4] and self.spejimai[8] == "X":
+                print("X laimejo")
+            elif self.spejimai[2] and self.spejimai[4] and self.spejimai[6] == "X":
+                print("X laimejo")
+            break
+
+# if self.spejimai[0] and self.spejimai[1] and self.spejimai[2] is isinstance(x, pasirinkimasX):
+
+# and self.spejimai[1] and self.spejimai[2]
 
 
 spejimai = Spejimai()
@@ -60,8 +77,10 @@ while True:
     pasirinkimas = int(input("Iveskite X ejima "))
     spejimai.prideti_X_spejima(pasirinkimas)
     spejimai.atvaizduoti()
+    spejimai.ar_laimejo_X()
     pasirinkimas = int(input("Iveskite O ejima "))
     spejimai.prideti_O_spejima(pasirinkimas)
     spejimai.atvaizduoti()
+    # spejimai.ar_laimejo_X()
 
 
